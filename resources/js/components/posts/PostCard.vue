a506ede73bc50f371c6c74cb9df4e92b1c9cc2d1
Elsti97 <elsti.shehati@hotmail.it>
2023-02-04 21:09:36 +0100

    exported last repo

---
+++ resources/js/components/posts/PostCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <h1 class="text-danger mb-4">Sono la pagina del singolo post: <h2 class="text-white d-inline">{{ posts.title }}</h2> </h1>
+
+        <h3>{{ posts.body }}</h3>
+        <p>Il piatto è un: {{ posts.category.name }}</p>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'PostCard',
+    components: {
+    },
+    data() {
+        return {
+            posts: null,
+        }
+    },
+    mounted() {
+        this.getPost();
+    },
+    methods: {
+        getPost() {
+            axios.get('http://localhost:8000/api/posts/' + this.$route.params.id)
+                .then((res) => {
+                    console.log(res.data);
+                    this.posts = res.data;
+                }).catch(err => {
+                    console.log(err)
+                })
+        }
+    }
+}
+</script>
